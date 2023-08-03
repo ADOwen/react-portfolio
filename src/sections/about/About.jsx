12471335897fe692d2534ded52ea51b2c96dda46
ADOwen <92840021+ADOwen@users.noreply.github.com>
2023-08-02 20:34:44 -0400

    Add card component and styles for about section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,11 +1,49 @@
-import './about.css'
+import AboutImage from '../../assets/about.jpg';
+import CV from '../../assets/cv.pdf';
+import {HiDownload} from 'react-icons/hi';
+import Card from '../../components/Card';
+import data from './data';
+import './about.css';
 
 const About = () => {
   return (
     <section id="about">
-      <h2>About</h2>
+      <div className="container about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="A portrait of myself" />
+          </div>
+        </div>
+        <div className="about__right">
+          <h2>About Me</h2>
+          <div className="about__cards">
+            {
+              data.map(item => {
+                return <Card key={item.id} className='about__card'>
+                  <span className='about__card-icon'>{item.icon}</span>
+                  <h5>{item.title}</h5>
+                  <small>{item.desc}</small>
+                </Card>
+              })
+            }
+          </div>
+          <p>
+            Building projects my clients love has always been my passion.
+            Being in the web development industry for 2 years and serving
+            happy clients worldwide, I'm always motivated to do more!
+          </p>
+          <p>
+            Hi, I'm a full-stack web developer. My top priority is to get your
+            business online the right way, giving you industry-standard design
+            and all the functionality you need to operate smoothly online. Get
+            in touch today with the details of your project let's get started!
+            Check out my resume below!
+          </p>
+          <a href={CV} download className='btn primary'>Download CV <HiDownload/></a>
+        </div>
+      </div>
     </section>
   )
-}
+};
 
-export default About
+export default About;
