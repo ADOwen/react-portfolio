01faeaef6899ce1140aef032adb0bebe8901b738
ADOwen <92840021+ADOwen@users.noreply.github.com>
2023-08-03 19:41:42 -0400

    Add Project section and Project component

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,9 +1,22 @@
 import './portfolio.css'
+import ProjectCategories from './ProjectCategories';
+import Projects from './Projects';
+import data from './data'
+import { useState } from 'react';
 
 const Portfolio = () => {
+  const [projects, setProjects ] = useState(data)
   return (
     <section id="portfolio">
-      <h2>Portfolio</h2>
+      <h2>Recent Projects</h2>
+      <p>
+        Check out some of my recent projects. Use the buttons
+        to toggle different categories.
+      </p>
+      <div className="portfolio__container">
+        <ProjectCategories/>
+        <Projects projects={projects}/>
+      </div>
     </section>
   )
 };
