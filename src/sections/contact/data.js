490efe9ba01e91f64225b5e4e053f077d8ff2266
ADOwen <92840021+ADOwen@users.noreply.github.com>
2023-08-27 22:31:17 -0400

    Add contact componenet links and contact data

---
+++ src/sections/contact/data.js
@@ -0,0 +1,15 @@
+import {HiOutlineMail} from 'react-icons/hi'
+import {RiMessengerLine} from 'react-icons/ri'
+import {FaWhatsapp} from 'react-icons/fa'
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:a.einhorn886@gmail.com'},
+    {id: 2, icon: <RiMessengerLine/>, link: 'https://www.messenger.com/t/Adiizzle'},
+    {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/13472636306'}
+]
+
+
+
+
+
+export default data;
