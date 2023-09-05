217a7dec2d6224811b20388fb56ec1fef0f7a8f7
ADOwen <92840021+ADOwen@users.noreply.github.com>
2023-09-05 16:18:37 -0400

    Implement hide floating navbar feature when idle

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./sections/header/Header";
 import About from "./sections/about/About";
 import Services from "./sections/services/Services";
 import Portfolio from "./sections/portfolio/Portfolio";
+import { useRef, useState, useEffect } from "react";
 import Testimonials from "./sections/testimonials/Testimonials";
 import FAQS from "./sections/faqs/FAQS";
 import Contact from "./sections/contact/Contact";
@@ -13,8 +14,41 @@ import { useThemeContext } from "./context/theme-context";
 
 const App = () => {
   const {themeState} = useThemeContext();
+  
+  const mainRef = useRef();
+
+  const [showFloatingNav, setShowFloatingNav ] = useState(true);
+  const [siteYPosition, setSiteYPosition] = useState(0);
+
+  const showFloatingNavHandler = () => {
+    setShowFloatingNav(true);
+  };
+
+  const hideFloatingNavHandler = () => {
+    setShowFloatingNav(false);
+  };
+
+  // check if floating nav should be shown or hidden
+  const floatingNavToggleHandler = () => {
+    // check if we scrolled up or down at least 20px
+    if (siteYPosition < mainRef?.current?.getBoundingClientRect().y - 20 || siteYPosition > mainRef?.current?.getBoundingClientRect().y + 20 ) {
+      showFloatingNavHandler();
+      console.log("did I run again?")
+    } else {
+      hideFloatingNavHandler()
+    }
+
+    setSiteYPosition(mainRef?.current?.getBoundingClientRect().y)
+  }
+
+  useEffect(() => {
+    const checkYPosition = setInterval(floatingNavToggleHandler, 1500);
+    return () => clearInterval(checkYPosition)
+  }, [siteYPosition])
+  
+
   return (
-    <main className={`${themeState.primary} ${themeState.background}`}>
+    <main className={`${themeState.primary} ${themeState.background}`} ref={mainRef}>
       <Navbar/>
       <Header/>
       <About/>
@@ -25,8 +59,7 @@ const App = () => {
       <Contact />
       <Footer />
       <Theme/>
-      <FloatingNav />
-
+      {showFloatingNav && <FloatingNav/>}
     </main>
   )
 };
